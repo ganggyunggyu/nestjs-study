16c9f8958d292838646f06db588f2df339687440
강경규 <qwzx16@naver.com>
2024-03-29 19:28:03 +0900

    chore: 주석 추가
    - 여기까지 테스트 완료
    - 여기부턴 JWT를 구현

--- nest-auth/src/app.controller.ts
+++ nest-auth/src/app.controller.ts
@@ -3,9 +3,17 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
+//
+
 @Controller()
 export class AppController {
   @UseGuards(LocalAuthGuard)
+  /**
+   * 로컬 가드
+   * @UseGuards(AuthGuard('local'))
+   * 이런 식으로 기입 해도 동작하지만 string을 기입하는 것은 좋지 않으므로
+   * 자체 클래스를 생성하여 전달하는 방법으로 많이 사용된다.
+   */
   @Post('auth/login')
   async login(@Body() requestUserInfo) {
     return requestUserInfo;
