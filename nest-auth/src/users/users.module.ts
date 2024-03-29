fd784999d76d9ec55ab7c8c0e1e951bd9b7b6de3
강경규 <qwzx16@naver.com>
2024-03-29 19:23:12 +0900

    feat: users 배열 및 user findOne 메서드 생성

--- nest-auth/src/users/users.module.ts
+++ nest-auth/src/users/users.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService],
+  //AuthService에서 사용하기 위해 내보내기
 })
 export class UsersModule {}
--- nest-auth/src/users/users.service.ts
+++ nest-auth/src/users/users.service.ts
@@ -1,4 +1,25 @@
+// users/users.service.ts
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entity
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'gang',
+      password: '123123',
+    },
+    {
+      userId: 2,
+      username: 'ko',
+      password: '1234',
+    },
+  ];
+  //실제로는 DB의 데이터를 사용할 것
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
