2ff58af5a02aacecec10981b5ddf440b6e56e97b
강경규 <qwzx16@naver.com>
2024-03-30 18:07:47 +0900

    test: .env test

--- nest-auth/src/app.module.ts
+++ nest-auth/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
