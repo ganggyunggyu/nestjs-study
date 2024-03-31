4472debe9c81c6e334e24f790eaffb0c4deac3e0
강경규 <qwzx16@naver.com>
2024-03-31 18:05:46 +0900

    feat: guard 별도로 분리하여 사용

--- nest-auth/src/app.controller.ts
+++ nest-auth/src/app.controller.ts
@@ -1,9 +1,7 @@
-import { Controller, Post, UseGuards, Injectable, Body } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Post, UseGuards, Body, Get } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
-
-@Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+import { LocalAuthGuard } from './auth/guard/local-auth.guard';
+import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -13,12 +11,17 @@ export class AppController {
   /**
    * 로컬 가드
    * @UseGuards(AuthGuard('local'))
-   * 이런 식으로 기입 해도 동작하지만 string을 기입하는 것은 좋지 않으므로
+   * 이런 식으로 기입 해도 동작하지만 그 자체를 string을 기입하는 것은 좋지 않으므로
    * 자체 클래스를 생성하여 전달하는 방법으로 많이 사용된다.
    */
   @Post('auth/login')
   async login(@Body() requestUserInfo) {
-    // return requestUserInfo;
-    return this.authService.login(requestUserInfo);
+    return this.authService.createAccessToken(requestUserInfo);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Body() userInfo) {
+    return userInfo;
   }
 }
